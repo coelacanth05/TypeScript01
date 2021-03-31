45205f5db88f6c4464319e4f945a973b40b7b1d8
kai <63350063+coelacanth05@users.noreply.github.com>
2021-03-31 21:58:33 +0900

    objectの型定義

--- src/object/alias.ts
+++ src/object/alias.ts
@@ -11,4 +11,35 @@ export default function typeAliasSample() {
   };
 
   console.log("Object object sample 5:", japan);
+
+  type Knight = {
+    hp: number;
+    sp: number;
+    weapon: string;
+    swordSkill: string;
+  };
+  type Wizard = {
+    hp: number;
+    mp: number;
+    weapon: string;
+    magicSkill: string;
+  };
+
+  type Adventurer = Knight | Wizard;
+  type Paladin = Knight & Wizard;
+
+  const adventurer1: Adventurer = {
+    hp: 100,
+    sp: 30,
+    weapon: "木の剣",
+    swordSkill: "3連切り"
+  };
+  const adventurer2: Adventurer = {
+    hp: 100,
+    mp: 30,
+    weapon: "木の杖",
+    swordSkill: "ファイアボール"
+  };
+  console.log(adventurer1);
+  console.log(adventurer2);
 }
